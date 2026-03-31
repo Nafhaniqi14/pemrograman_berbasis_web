9e0c904ccdd817e1ddf3023e28bc2ba6b6ccd9b7
Nafhaniqi14 <nafhanhaqiqi144@gmail.com>
2026-03-31 20:19:45 +0700

    persiapan evaluasi

--- Tugas_Pertemuan_6/latihan_diskon.php
+++ Tugas_Pertemuan_6/latihan_diskon.php
@@ -1,36 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Latihan Diskon</title>
-</head>
-<body>
-    <form method="post" action="">
-        NPM : <input type="text" name="npm"><br>
-    Nama : <input type="text" name="nama"><br>
-    Prodi : <input type="text" name="prodi"><br>
-    Semester : <input type="number" name="semester"><br>
-    Biaya UKT : <input type="number" name="BUKT"><br>
-    <input type="submit" value="proses"> <br>
-        <?php
-    $var_nama = $_POST['nama'];
-    $var_npm = $_POST['npm'];
-    $var_prodi = $_POST['prodi'];
-    $var_smt = $_POST['semester'];
-    $var_bukt = $_POST['BUKT'];
-    echo"NPM :  {$var_npm} <br>";
-    echo "Nama : {$var_nama} <br>";
-    echo "Semester : {$var_smt} <br>";
-    echo"Biaya UKT : {$var_bukt} <br>";
-    if  ($var_bukt >= 5000000 && $var_smt >8) {
-        echo "Diskon : 15% <br>" ;
-        echo "yang harus dibayar : " . $var_bukt-$var_bukt*0.15 ;
-      } else if($var_bukt >= 5000000){
-        echo "Diskon : 10% <br>" ;
-        echo "yang harus dibayar : " . $var_bukt-$var_bukt*0.10 ;
-      }
-    ?>
-    </form>
-</body>
-</html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Latihan Diskon</title>
+    </head>
+    <body>
+        <form method="post" action="">
+            NPM : <input type="text" name="npm"><br>
+            Nama : <input type="text" name="nama"><br>
+            Prodi : <input type="text" name="prodi"><br>
+            Semester : <input type="number" name="semester"><br>
+            Biaya UKT : <input type="number" name="BUKT"><br>
+            <input type="submit" value="proses"> <br>
+            <?php
+            $var_nama = $_POST['nama']??'';
+            $var_npm = $_POST['npm']??'';
+            $var_prodi = $_POST['prodi']??'';
+            $var_smt = $_POST['semester']??'';
+            $var_bukt = $_POST['BUKT']??'';
+            if  ($var_bukt >= 5000000 && $var_smt >8) {
+                echo"NPM :  {$var_npm} <br>";
+                echo "Nama : {$var_nama} <br>";
+                echo "Semester : {$var_smt} <br>";
+                echo"Biaya UKT : {$var_bukt} <br>";
+                echo "Diskon : 15% <br>" ;
+                echo "yang harus dibayar : " . $var_bukt-$var_bukt*0.15 ;
+                } else if($var_bukt >= 5000000){
+                    echo"NPM :  {$var_npm} <br>";
+                    echo "Nama : {$var_nama} <br>";
+                    echo "Semester : {$var_smt} <br>";
+                    echo "Diskon : 10% <br>" ;
+                    echo "yang harus dibayar : " . $var_bukt-$var_bukt*0.10 ;
+                    echo"Biaya UKT : {$var_bukt} <br>";
+                    }
+                    else {
+                        echo"NPM :  {$var_npm} <br>";
+                        echo "Nama : {$var_nama} <br>";
+                        echo "Semester : {$var_smt} <br>";
+                        echo "yang harus dibayar : {$var_bukt} <br>"  ;
+                        echo"Biaya UKT : {$var_bukt} <br>";
+                        }
+                        ?>
+        </form>
+    </body>
+    </html>
--- Tugas_Pertemuan_6/latihan_nilai.php
+++ Tugas_Pertemuan_6/latihan_nilai.php
@@ -1,36 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan Nilai</title>
-</head>
+  </head>
 <body>
     <form method="post" action="">
-    Nama : <input type="text" name="nama"><br>
-    Nilai : <input type="number" name="nilai"><br>
-    <input type="submit" value="proses"> <br>
-        <?php
-    $var_nama = $_POST['nama'];
-    $var_nilai = $_POST['nilai'];
-    echo "Nama : ". $var_nama ."<br>";
-    echo "Nilai : ". $var_nilai."<br>";
+      Nama : <input type="text" name="nama"><br>
+      Nilai : <input type="number" name="nilai"><br>
+      <input type="submit" value="proses"> <br>
+      <?php
+    $var_nama = $_POST['nama']??'';
+    $var_nilai = $_POST['nilai']??'';
+    echo "Nama :  {$var_nama} <br>";
+    echo "Nilai : {$var_nilai} <br>";
     if ($var_nilai >= 80 && $var_nilai <= 100) {
-        echo "Predikat :  A <br> Status : Lulus";
+      echo "Predikat :  A <br> Status : Lulus";
       } else if ($var_nilai >= 70) {
         echo "Predikat :  B <br> Status : Lulus";
-      }else if ($var_nilai >= 60) {
-        echo "Predikat :  C <br> Status : Lulus";
-      } 
-     else if ($var_nilai >= 50 ) {
-        echo "Predikat :  D <br> Status : Lulus";
-      }  else if ($var_nilai>= 0 ) {
-        echo "Predikat :  E <br> Status : Tidak Lulus";
+        }else if ($var_nilai >= 60) {
+          echo "Predikat :  C <br> Status : Lulus";
+          } 
+          else if ($var_nilai >= 50 ) {
+            echo "Predikat :  D <br> Status : Lulus";
+            }  else if ($var_nilai>= 0 ) {
+              echo "Predikat :  E <br> Status : Tidak Lulus";
       } 
       else {
-        echo "Predikat :  Tidak";
-      }
-    ?>
+        echo "Predikat :  Tidak Valid";
+        }
+        ?>
     </form>
-</body>
-</html>
+  </body>
+  </html>
